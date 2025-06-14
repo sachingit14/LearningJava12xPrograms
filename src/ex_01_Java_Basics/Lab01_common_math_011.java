5fc680ab8aaa2570b6c08a96f11ea4d81c0fe0f6
sachingit14 <sachinyadav98196@gmail.com>
2025-06-14 15:37:36 +0530

    basic java

---
+++ src/ex_01_Java_Basics/Lab01_common_math_011.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Lab01_common_math_011 {
+
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(3/2); //pirntln -> Integer part-> No decimal
+    }
+}
