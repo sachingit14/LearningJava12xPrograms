95d0bb5ced2e8d09c5e5da224334a3e04ef9f3ab
sachingit14 <sachinyadav98196@gmail.com>
2025-06-15 01:52:49 +0530

    basic java

---
+++ src/ex_01_Java_Basics/Lab02_variables_16.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class Lab02_variables_16 {
+
+    public static void main(String[] args) {
+        // public, class , static.. -- Keyword
+        // data_type --variable name -- variable value
+        int a =10;
+        // int -- data type -- type of container
+        // a -- variable name -- identifier
+        // = -- Assignment operator
+        // 10 -- variable value
+        a =65+1;
+        System.out.println(a);
+        // it will print latest value ie 66
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab03_numeric_data_type_22.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab03_numeric_data_type_22 {
+    public static void main(String[] args) {
+        byte a =127;
+        int b= 2147483647;
+        short c = 32767;
+        char d = 'A';
+        long e = 9876543251L;
+        float f = 3.14f;
+        double g = 3.146541869464;
+
+    }
+}
