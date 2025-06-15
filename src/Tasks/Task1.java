aef30c6cb077048aaa002ff8b95b61597f3ab881
sachingit14 <sachinyadav98196@gmail.com>
2025-06-16 00:33:53 +0530

    basic java

---
+++ src/Tasks/Task1.java
@@ -0,0 +1,23 @@
+package Tasks;
+
+public class Task1 {
+
+    //Table of 5 need to print,  (only use of printF)
+    //int num = 5;
+    //5×1=5
+    //5×2 = 10
+    //….
+    //5×10 =50
+
+    public static void main(String[] args) {
+        int num =5;
+
+        System.out.printf("%d*1=%d",num,num*1).println();
+        System.out.printf("%d*2=%d",num,num*2).println();
+        System.out.printf("%d*3=%d",num,num*3).println();
+        System.out.printf("%d*4=%d",num,num*4).println();
+        System.out.printf("%d*5=%d",num,num*5).println();
+
+
+    }
+}
