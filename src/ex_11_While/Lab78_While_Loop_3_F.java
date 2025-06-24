ab059e536785d3034f43ca29c676604989b992b5
sachingit14 <sachinyadav98196@gmail.com>
2025-06-25 02:02:45 +0530

    basic java

---
+++ src/ex_11_While/Lab78_While_Loop_3_F.java
@@ -0,0 +1,13 @@
+package ex_11_While;
+
+public class Lab78_While_Loop_3_F {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i > 0) {       //condition flase
+            //
+            System.out.println("Enter");
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ src/ex_11_While/Lab79_While.java
@@ -0,0 +1,9 @@
+package ex_11_While;
+
+public class Lab79_While {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Hello, Pramod");
+        }
+    }
+}
