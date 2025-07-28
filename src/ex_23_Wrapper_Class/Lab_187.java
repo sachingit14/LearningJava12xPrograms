e42373317098f67070ab73f5ae3e3284033b0cad
sachingit14 <sachinyadav98196@gmail.com>
2025-07-28 22:14:07 +0530

    basic java

---
+++ src/ex_23_Wrapper_Class/Lab_187.java
@@ -0,0 +1,13 @@
+package ex_23_Wrapper_Class;
+
+public class Lab_187 {
+
+}
+class ATB{
+    String name;
+
+    void print(){
+        System.out.println("Hi, name");
+    }
+
+}
---
+++ src/ex_23_Wrapper_Class/Lab_189_Wrapper_class.java
@@ -0,0 +1,17 @@
+package ex_23_Wrapper_Class;
+
+public class Lab_189_Wrapper_class {
+}
+class ATB1{
+    String name;
+    Long phone;
+    Integer salary;
+    Float GST;
+    Boolean isMarried;
+
+    //int -> data Type
+    // Integer - class - who object can be created.
+
+    // Why Java is not pure OOPs? - Becoz -> primitive data types.
+
+}
